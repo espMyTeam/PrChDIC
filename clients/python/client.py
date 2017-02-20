9abfe649c907af169404d579f4f9685a392de809
beaukams <kamstelecom@hotmail.com>
2017-02-20 00:01:01 +0000

    client python

---
+++ clients/python/client.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+#-*- coding:utf-8 -*-
+
+class ChatUser:
+	def __init__(self, pseudo):
+
+	def subscribe(self):
+
+	def unsubscriber(self):
+
+	def postMessage(self):
+
+	def createChatRoom(self):
+
+	def removeChatRoom(self):
+
+	
